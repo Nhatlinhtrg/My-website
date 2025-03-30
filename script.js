e57345246881e99e9dde324f5859a1d82af66612
Linh <truongthinhatlinh2408@gmail.com>
2025-03-30 20:36:38 +0700

    Update website

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// JavaScript for opening and closing sidebar
+// JavaScript for opening and closing sidebar (if needed)
 function openNav() {
     document.querySelector('.sidebar').classList.add('open');
-    document.querySelector('.main-content').style.marginLeft = "250px";
+    document.querySelector('.main-content').style.marginLeft = "300px";
 }
 
 function closeNav() {
     document.querySelector('.sidebar').classList.remove('open');
     document.querySelector('.main-content').style.marginLeft = "0";
 }
+
